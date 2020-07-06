cb6579c9e73bbd1e95646f0072efeb4a535c5ed2
Tanesh <35836662+FireMonkey92@users.noreply.github.com>
2020-07-06 13:50:53 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,23 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Opens in new tab
-        </a>
         <br />
-        <a rel="noopener noreferrer" className="App-link" target="_blank" href="https://helenzys.com/">link should open in new tab</a>
+
+        <a href="market://details?id=com.vmoksha.biomag" target="_blank">com.vmoksha.biomag</a>
         <br />
-        <a rel="noopener noreferrer" className="App-link" target="_self" href="https://helenzys.com/">link should not open in new tab</a>
+        <a href="market://details?id=com.vmoksha.biomag.3D.prod" target="_blank">com.vmoksha.biomag.3D.prod</a>
         <br />
-        <a rel="noopener noreferrer" className="App-link" href="https://helenzys.com/">link should not open in new tab</a>
+        <br />
+
+        <a href="https://play.google.com/store/apps/details?id=com.vmoksha.biomag" target="_blank" rel="external">FROM BROWSER LINK com.vmoksha.biomag</a>
+        <br />
+        <a href="https://play.google.com/store/apps/details?id=com.vmoksha.biomag.3D.prod" target="_blank" rel="external">FROM BROWSER LINK com.vmoksha.biomag.3D.prod</a>
       </header>
     </div>
   );
